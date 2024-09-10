12db42d2010605e784a0204c11efec8863e3efa5
kushal sarkar <kushalsarkar051@gmail.com>
2024-09-10 10:38:21 +0530

    Add files via upload

---
+++ simpleinrest.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// int main(){
+  
+//     float p;
+//     printf("ENTER THE VALUE OF P : ");
+//     scanf("%f",&p);
+//     float r;
+//     printf("ENTER THE VALUE OF R : ");
+//     scanf("%f",&r);
+//     float t;
+//     printf("ENTER THE VALUE OF T : ");
+//     scanf("%f",&t);
+//     float si = (p*r*t)/100;
+//     printf("%f",si);
+//     return 0;
+//     }
---
+++ straightlinr.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// int main(){
+//     float x1,x2,x3,y1,y2,y3,;
+//     printf("x1 : ");
+//     scanf("%f",&x1);
+//      printf("x2 : ");
+//     scanf("%f",&x2);
+//      printf("x3 : ");
+//     scanf("%f",&x3);
+//      printf("y1 : ");
+//     scanf("%f",&y1);
+//      printf("y2 : ");
+//     scanf("%f",&y2);
+//      printf("y3 : ");
+//     scanf("%f",&y3);
+// float m1,m2;
+// m1 = (y2-y1)/(x2-x1)
+// m2 = (y3-y2)/(x3-x2)
+// if(m1==0 && m2==0)
+// printf("NOT GAY");
+// else
+// printf("gay");
+// return 0;
+// }
