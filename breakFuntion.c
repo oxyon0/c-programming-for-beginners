2eb92e565d94c8c9388f90c21a926206f715a604
kushal sarkar <kushalsarkar051@gmail.com>
2024-09-13 11:38:13 +0530

    Add files via upload

---
+++ breakFuntion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+int n;
+printf("enter no. : ");
+scanf("%d",&n);
+int a = 0;
+for(int i = 2;i <=n-1;i++){
+if(n%i==0)
+printf("\nno. is composite");
+break;
+}
+if(n==1)
+printf("neither prime nor composite");
+else if(a==0)
+printf("prime no.");
+return 0;
+}
+
