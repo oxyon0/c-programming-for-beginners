9c5afbc9cd42fa067c14ce32b067c532a52b6bfe
kushal sarkar <kushalsarkar051@gmail.com>
2024-11-14 00:22:44 +0530

    Update muliple.c

--- loops/muliple.c
+++ loops/muliple.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
-// int main(){
-//     int n;
-//     printf("enter no.");
-//     scanf("%d",&n);
-//     int multiple = 1;
-//     for(int i = 1;i<=10;i++){
-//         multiple = i*n;
-//         printf("\nmulyiple are : %d",multiple);
-//     }
-//     return 0;
-// }
+ #include <stdio.h>
+ int main(){
+     int n;
+     printf("enter no.");
+     scanf("%d",&n);
+     int multiple = 1;
+]     for(int i = 1;i<=10;i++){
+         multiple = i*n;
+         printf("\nmulyiple are : %d",multiple);
+     }
+     return 0;
+ }
