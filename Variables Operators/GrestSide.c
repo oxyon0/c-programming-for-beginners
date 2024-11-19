7634ed1902072ce43b5a76b4b80fc6fa5eb91d03
kushal sarkar <kushalsarkar051@gmail.com>
2024-11-19 20:22:52 +0530

    Update GrestSide.c

--- Variables Operators/GrestSide.c
+++ Variables Operators/GrestSide.c
@@ -1,18 +1,18 @@
-// #include <stdio.h>
-// int main(){
-//     int ab,bc,ca;
-//     printf("enter the ab : ");
-//     scanf("%d",&ab);
-//     printf("enter the bc : ");
-//     scanf("%d",&bc);
-//     printf("enter the ca : ");
-//     scanf("%d",&ca);
-//  if(ab>bc && ab>ca)
-//  printf("side ab is best ");
-//  if(bc>ab && bc>ca)
-//  printf("side bc is best");
-//  else
-//  printf("side ca is best");
-//  return 0;
+#include <stdio.h>
+int main(){
+    int ab,bc,ca;
+    printf("enter the ab : ");
+    scanf("%d",&ab);
+    printf("enter the bc : ");
+    scanf("%d",&bc);
+    printf("enter the ca : ");
+    scanf("%d",&ca);
+ if(ab>bc && ab>ca)
+ printf("side ab is best ");
+ if(bc>ab && bc>ca)
+ printf("side bc is best");
+ else
+ printf("side ca is best");
+ return 0;
 
-// }
+}
