520ac445c2501acd8df68377c88997beecdbef18
kushal sarkar <kushalsarkar051@gmail.com>
2024-11-25 12:57:53 +0530

    Update factorial.c

--- loops/factorial.c
+++ loops/factorial.c
@@ -1,12 +1,12 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     printf("enter no. : ");
-//     scanf("%d",&n);
-//     int factorial = 1;
-//     for(int i=1;i<n;i++){
-//         factorial=i*(n%10);  
-//     }
-//      printf("\n %d",factorial);
-//     return 0;
-// }
+ #include<stdio.h>
+ int main(){
+     int n;
+     printf("enter no. : ");
+     scanf("%d",&n);
+    int factorial = 1;
+     for(int i=1;i<n;i++){
+         factorial=i*(n%10);  
+     }
+      printf("\n %d",factorial);
+     return 0;
+ }
